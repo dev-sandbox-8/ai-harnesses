84e813abbcaba8426e02223882556735eee2caf0
Selwyn Leeke <selwyn.leeke@gmail.com>
2026-06-24 06:59:42 -0700

    Updated settings

--- scripts/discord-notify.sh
+++ scripts/discord-notify.sh
@@ -3,11 +3,12 @@ IFS=$'\n'
 
 PROMPT=$1
 # JSON=$(cat)
-# JSON=$(cat /dev/stdin)
-# TOOL=$(echo $JSON | jq -r '.tool_name')
-# COMMAND=$(echo $JSON | jq -r '.tool_input.command')
+# JSON=$(read -t 1)
+JSON=$(timeout 1 cat /dev/stdin)
+TOOL=$(echo $JSON | jq -r '.tool_name')
+COMMAND=$(echo $JSON | jq -r '.tool_input.command')
 
-PROMPT="Command: $PROMPT\n\nContent:\n\n$JSON" #Using tool: $TOOL\nRunning command: $COMMAND"
+PROMPT="$PROMPT\n\nUsing tool: $TOOL\nRunning command: $COMMAND\n\nContent:\n\n$JSON"
 
 DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/1517018133011894395/eklbj-ntpKkXcbsgW9etVK7bCIhyj5qYEGvXE39YyVNHgVsQmXQFdS2BhcghIENWp07-"
 
