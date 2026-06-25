c4fdbd9ae2f5d6e6912b37046f6967135d2b4456
Selwyn Leeke <selwyn.leeke@gmail.com>
2026-06-25 14:31:41 -0700

    Simplified discord notification script

--- scripts/discord-notify.sh
+++ scripts/discord-notify.sh
@@ -4,11 +4,11 @@ IFS=$'\n'
 PROMPT=$1
 # JSON=$(cat)
 # JSON=$(read -t 1)
-JSON=$(timeout 1 cat /dev/stdin)
-TOOL=$(echo $JSON | jq -r '.tool_name')
-COMMAND=$(echo $JSON | jq -r '.tool_input.command')
+# JSON=$(timeout 1 cat /dev/stdin)
+# TOOL=$(echo $JSON | jq -r '.tool_name')
+# COMMAND=$(echo $JSON | jq -r '.tool_input.command')
 
-PROMPT="$PROMPT\n\nUsing tool: $TOOL\nRunning command: $COMMAND\n\nContent:\n\n$JSON"
+# PROMPT="$PROMPT\n\nUsing tool: $TOOL\nRunning command: $COMMAND\n\nContent:\n\n$JSON"
 
 DISCORD_WEBHOOK_URL="https://discord.com/api/webhooks/1517018133011894395/eklbj-ntpKkXcbsgW9etVK7bCIhyj5qYEGvXE39YyVNHgVsQmXQFdS2BhcghIENWp07-"
 
